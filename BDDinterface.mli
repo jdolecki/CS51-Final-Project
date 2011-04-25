f317e1382c201e49447958f11b427aebc450027c
Jakub Dolecki <jakub.dolecki2@gmail.com>
2011-04-24 23:35:18 -0400

    added comments

---
+++ BDDinterface.mli
@@ -0,0 +1,69 @@
+(*
+ * Team BDD 
+ * Jakub Dolecki, Sean Murphy, Michael Cioffi
+ * Spring, 2011
+ * 
+ * Binary Decision Diagram interface
+ * 
+ * Binary decision diagrams are data structures that are used to represent
+ * boolean functions. This project aims to implement a module that can be
+ * utilized in creating BDDs and manipulating them.
+ *
+ *)
+
+(* An interface for BDD module *)
+module type BDD =
+  sig
+    type bdd (* abstract type for bdd *)
+  
+    type expression (* abstract type for boolean expression *)
+
+    type operation (* abstract type for boolean operators *)
+
+    (*
+     * make builds a node out of an expression and ensures the bdd is reduced
+     * as outlined in ¡°An Introduction to Binary Decision Diagrams¡± 
+     * by Andersen.
+     *)
+    val make : int -> bdd -> bdd -> bdd
+
+    (* 
+     * build takes an expression and returns its binary decision diagram. this
+     * function actually builds a bdd from an expression
+     *)
+    val build: expression -> bdd
+    
+    (*
+     * apply will take a boolean operator and two bdds and return a new bdd 
+     * that is the result of the specified operation
+     *)
+    val apply: op -> bdd -> bdd -> bdd
+
+   (*
+    * sat_count will return the number of solutions to the Boolean function
+    * given f(x) = 1
+    *)
+    val sat_count: bdd -> float
+
+    (*
+     * any_sat returns any path to a truth assignment
+     *)
+    val any_sat: bdd -> (int * expression) list
+
+    (*
+     * all_sat returns all satisfying assignments
+     *)
+    val all_sat: bdd -> (int * expression) list list
+
+    (* TO DO Cool things like print and other extra features 
+
+    val print: bdd -> unit
+
+    val restrict: bdd -> assignment -> bdd
+
+    val random_sat: bdd -> int list
+    *)
+
+  end
+
+
